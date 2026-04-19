77e726e8a1e2a1416e8466be69dbd899a1a11fa0
hbhdfjy <hbhdfjy@163.com>
2026-04-19 14:53:43 +0800

    fix(layout): codex gpt-5 restore right pane scrolling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import { installWheelScrollAssist } from './lib/scrollAssist'
 import './index.css'
-
-installWheelScrollAssist()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
